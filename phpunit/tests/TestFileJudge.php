020f6207ab36b2af2b285a17946f4cd733406386
Tim Tegeler <tim@tegeler.me>
2016-06-16 09:37:13 +0200

    Added more tests to reach 100% coverage

--- phpunit/tests/TestFileJudge.php
+++ phpunit/tests/TestFileJudge.php
@@ -13,16 +13,55 @@ class TestFileJudge extends \PHPUnit_Framework_TestCase
             ->addAssertedMediaTypeSubtype("jpg")->judge(__DIR__ . "/../assets/image.png");
 
         $this->assertTrue($fileJudgement->isPassed());
+        $this->assertFalse($fileJudgement->isMediaTypeFailed());
+        $this->assertFalse($fileJudgement->isMediaTypeSubtypeFailed());
+        $this->assertEquals(["image"], $fileJudgement->getAssertedMediaTypes());
+        $this->assertEquals(["png", "jpg"], $fileJudgement->getAssertedMediaTypeSubtypes());
         $this->assertEquals("image", $fileJudgement->getActualMediaType());
         $this->assertEquals("png", $fileJudgement->getActualMediaTypeSubtype());
     }
 
-    public function testGreater()
+    public function testMediaTypeFailed()
+    {
+        $fileJudgement = (new FileJudge())
+            ->addAssertedMediaType("text")
+            ->addAssertedMediaTypeSubtype("html")
+            ->addAssertedMediaTypeSubtype("xml")->judge(__DIR__ . "/../assets/image.png");
+
+        $this->assertFalse($fileJudgement->isPassed());
+        $this->assertTrue($fileJudgement->isMediaTypeFailed());
+        $this->assertTrue($fileJudgement->isMediaTypeSubtypeFailed());
+        $this->assertEquals(["text"], $fileJudgement->getAssertedMediaTypes());
+        $this->assertEquals(["html", "xml"], $fileJudgement->getAssertedMediaTypeSubtypes());
+        $this->assertEquals("image", $fileJudgement->getActualMediaType());
+        $this->assertEquals("png", $fileJudgement->getActualMediaTypeSubtype());
+    }
+
+    public function testFileSize()
     {
         $fileJudgement = (new FileJudge())
             ->setAssertedMaxFileSize(2479123)
             ->setAssertedMinFileSize(1000)->judge(__DIR__ . "/../assets/image.png");
         $this->assertTrue($fileJudgement->isPassed());
+        $this->assertFalse($fileJudgement->isMaxFileSizeFailed());
+        $this->assertFalse($fileJudgement->isMinFileSizeFailed());
+        $this->assertEquals(2479123, $fileJudgement->getAssertedMaxFileSize());
+        $this->assertEquals(1000, $fileJudgement->getAssertedMinFileSize());
+        $this->assertEquals(5447, $fileJudgement->getActualFileSize());
+        $this->assertEquals(5447, $fileJudgement->getActualFileSize());
+    }
+
+    public function testFileSizeFailed()
+    {
+        $fileJudgement = (new FileJudge())
+            ->setAssertedMaxFileSize(5000)
+            ->setAssertedMinFileSize(6000)->judge(__DIR__ . "/../assets/image.png");
+        $this->assertFalse($fileJudgement->isPassed());
+        $this->assertTrue($fileJudgement->isMaxFileSizeFailed());
+        $this->assertTrue($fileJudgement->isMinFileSizeFailed());
+        $this->assertEquals(5000, $fileJudgement->getAssertedMaxFileSize());
+        $this->assertEquals(6000, $fileJudgement->getAssertedMinFileSize());
+        $this->assertEquals(5447, $fileJudgement->getActualFileSize());
         $this->assertEquals(5447, $fileJudgement->getActualFileSize());
     }
 
--- phpunit/tests/TestImageJudge.php
+++ phpunit/tests/TestImageJudge.php
@@ -11,6 +11,10 @@ class TestImageJudge extends \PHPUnit_Framework_TestCase
             ->setAssertedMaxWidth(100)
             ->setAssertedMinWidth(99)->judge(__DIR__ . "/../assets/image.png");
         $this->assertTrue($imageJudgement->isPassed());
+        $this->assertFalse($imageJudgement->isMaxWidthFailed());
+        $this->assertFalse($imageJudgement->isMinWidthFailed());
+        $this->assertEquals(100, $imageJudgement->getAssertedMaxWidth());
+        $this->assertEquals(99, $imageJudgement->getAssertedMinWidth());
         $this->assertEquals(100, $imageJudgement->getActualWidth());
     }
 
@@ -20,6 +24,10 @@ class TestImageJudge extends \PHPUnit_Framework_TestCase
             ->setAssertedMaxHeight(101)
             ->setAssertedMinHeight(99)->judge(__DIR__ . "/../assets/image.png");
         $this->assertTrue($imageJudgement->isPassed());
+        $this->assertFalse($imageJudgement->isMaxHeightFailed());
+        $this->assertFalse($imageJudgement->isMinHeightFailed());
+        $this->assertEquals(101, $imageJudgement->getAssertedMaxHeight());
+        $this->assertEquals(99, $imageJudgement->getAssertedMinHeight());
         $this->assertEquals(100, $imageJudgement->getActualHeight());
     }
 
@@ -28,6 +36,7 @@ class TestImageJudge extends \PHPUnit_Framework_TestCase
         $imageJudgement = (new ImageJudge())
             ->setAssertedMinWidth(101)->judge(__DIR__ . "/../assets/image.png");
         $this->assertFalse($imageJudgement->isPassed());
+        $this->assertTrue($imageJudgement->isMinWidthFailed());
         $this->assertEquals(100, $imageJudgement->getActualWidth());
     }
 
@@ -36,6 +45,7 @@ class TestImageJudge extends \PHPUnit_Framework_TestCase
         $imageJudgement = (new ImageJudge())
             ->setAssertedMinHeight(101)->judge(__DIR__ . "/../assets/image.png");
         $this->assertFalse($imageJudgement->isPassed());
+        $this->assertTrue($imageJudgement->isMinHeightFailed());
         $this->assertEquals(100, $imageJudgement->getActualHeight());
     }
 
@@ -44,6 +54,7 @@ class TestImageJudge extends \PHPUnit_Framework_TestCase
         $imageJudgement = (new ImageJudge())
             ->setAssertedMaxWidth(99)->judge(__DIR__ . "/../assets/image.png");
         $this->assertFalse($imageJudgement->isPassed());
+        $this->assertTrue($imageJudgement->isMaxWidthFailed());
         $this->assertEquals(100, $imageJudgement->getActualWidth());
     }
 
@@ -52,6 +63,7 @@ class TestImageJudge extends \PHPUnit_Framework_TestCase
         $imageJudgement = (new ImageJudge())
             ->setAssertedMaxHeight(99)->judge(__DIR__ . "/../assets/image.png");
         $this->assertFalse($imageJudgement->isPassed());
+        $this->assertTrue($imageJudgement->isMaxHeightFailed());
         $this->assertEquals(100, $imageJudgement->getActualHeight());
     }
 
